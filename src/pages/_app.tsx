c176ea7cfafd460cc3222c1c270560664845ac7a
Steven Conaway <steven.conaway@icloud.com>
2022-06-09 16:57:18 -0600

    fix(/): fix some TS and lighthouse complaints

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import { AppProps } from 'next/app';
-import '@/styles/global.css';
+import Head from 'next/head';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import '@/styles/global.css';
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <title>Steven Conaway</title>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      <link rel="icon" href="/favicon.ico" key="shortcutIcon" />
+      <link rel="shortcut icon" href="/favicon.ico" key="shortcutIcon" />
+      {/* <link rel="manifest" href="/manifest.json" /> */}
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
+
+export default App;
