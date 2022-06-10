5cd67a8f8a3061e5002018e1098fad78bb98f413
Steven Conaway <steven.conaway@icloud.com>
2022-06-09 21:48:36 -0600

    fix(/): open socials in new tab

--- src/components/SocialIcon.tsx
+++ src/components/SocialIcon.tsx
@@ -7,7 +7,13 @@ type Props = {
 const SocialIcon = (props: Props) => {
   return (
     <div className="p-2 text-4xl transition-transform border border-solid rounded-full hover:scale-110 text-slate-300 border-slate-300 hover:border-slate-500 hover:text-slate-500">
-      <a href={props.url} className="">
+      <a
+        href={props.url}
+        className=""
+        // open on new tab
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         {props.icon}
       </a>
     </div>
