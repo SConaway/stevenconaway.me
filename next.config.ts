068ae55d40953707e3e3b6f9d3ff00f023ddf3cf
Steven Conaway <steven.conaway@icloud.com>
2025-12-10 10:27:33 -0800

    fix(config): add allowed image qualities

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    qualities: [20, 40, 75]
+  }
 };
 
 export default nextConfig;
